c7de268e61c5dce75f8cbe3fc42efe6bd7e2c676
DiogoSPinheiro <43498725+DiogoSPinheiro@users.noreply.github.com>
2018-10-01 14:55:28 -0300

    Add files via upload
    Inserção do hash com o programa principal.

--- ProgramaModulo/cryptnum.c
+++ ProgramaModulo/cryptnum.c
@@ -6,10 +6,11 @@
 #include <linux/uaccess.h>          // FUncao de copiar para o usuario
 #include <linux/mutex.h>
 #include <linux/moduleparam.h>
+#include <crypto/internal/hash.h>	//para o hash
 
 #define  DEVICE_NAME "cryptnum"    // /proc/cryptnum
 #define  CLASS_NAME  "ebb"        // declarando classe de que eh um dispositivo de caractere
-
+#define SHA256_LENGTH 32	// tamanho da mensagem
 
 
 MODULE_LICENSE("GPL");            // Licensa
@@ -44,6 +45,59 @@ static struct file_operations fops =
     .write = dev_write,      // .write eh tratado como dev_write
     .release = dev_release,  // .release eh tratado como dev_release
 };
+
+// Mostrar hash //
+static void show_hash_result(char * plaintext, char * hash_sha256)
+{
+    int i;
+    char str[SHA256_LENGTH*2 + 1];
+
+    pr_info("sha256 test for string: \"%s\"\n", plaintext);
+    for (i = 0; i < SHA256_LENGTH ; i++)
+        sprintf(&str[i*2],"%02x", (unsigned char)hash_sha256[i]);
+    str[i*2] = 0;
+    pr_info("%s\n", str);
+}
+
+int cryptosha256(void)
+{
+    char * plaintext = myKey;
+    char hash_sha256[SHA256_LENGTH];
+    struct crypto_shash *sha256;
+    struct shash_desc *shash;
+
+    sha256 = crypto_alloc_shash("sha256", 0, 0);
+    if (IS_ERR(sha256))
+        return -1;
+
+    shash =
+        kmalloc(sizeof(struct shash_desc) + crypto_shash_descsize(sha256),
+                GFP_KERNEL);
+    if (!shash)
+        return -ENOMEM;
+
+    shash->tfm = sha256;
+    shash->flags = 0;
+
+    if (crypto_shash_init(shash))
+        return -1;
+
+    if (crypto_shash_update(shash, plaintext, strlen(plaintext)))
+        return -1;
+
+    if (crypto_shash_final(shash, hash_sha256))
+        return -1;
+
+    kfree(shash);
+    crypto_free_shash(sha256);
+
+    show_hash_result(plaintext, hash_sha256);
+
+    return 0;
+}
+
+
+
 
 // *** Iniciando o modulo *** //
 
@@ -139,6 +193,7 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
             
         case 'c':
             sprintf(message, "Criptografia");
+		cryptosha256();
             break;
             
         case 'd':
